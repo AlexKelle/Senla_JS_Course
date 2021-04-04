035b847a21096d77c2a266bcccd111fe3fab1cdb
Alex <alexandra_kelle@senla.eu>
2021-04-04 18:32:16 +0200

    feat: lection 6 has been added

---
+++ lection6/index.js
@@ -0,0 +1,43 @@
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  const li = document.createElement("li");
+  const inputValue = document.getElementById("myInput").value;
+  const t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  const button = document.createElement("BUTTON");
+  const txt = document.createTextNode("\u00D7");
+  button.className = "close";
+  button.appendChild(txt);
+  li.appendChild(button);
+
+  for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      let div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+const list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Click on a close button to hide the current list item
+const close = document.getElementsByClassName("close");
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    let div = this.parentElement;
+    div.style.display = "none";
+  }
+}
